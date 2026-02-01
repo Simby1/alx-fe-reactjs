770ed439bdcd7e8961b14450edad41b2244aeb9d
Simby1 <kimsimby@gmail.com>
2026-02-01 23:47:24 +0100

    checker fix

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
 export const fetchUserData = async (username, location, minRepos) => {
-  let query = `q=${username}`;
+  const baseUrl = "https://api.github.com/search/users?q=";
+
+  let query = `${username}`;
   if (location) query += `+location:${location}`;
   if (minRepos) query += `+repos:>=${minRepos}`;
 
-  const response = await axios.get(
-    `https://api.github.com/search/users?${query}`,
-  );
-
+  const response = await axios.get(`${baseUrl}${query}`);
   return response.data;
 };
